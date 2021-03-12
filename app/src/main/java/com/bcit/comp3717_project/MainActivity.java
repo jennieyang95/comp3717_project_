8c3dd6572f25d945a7fdecaac0a6ce63707d47fc
dafu2020 <xlu49@my.bcit.ca>
2021-03-11 17:00:27 -0800

    Initial commit

---
+++ app/src/main/java/com/bcit/comp3717_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bcit.comp3717_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
